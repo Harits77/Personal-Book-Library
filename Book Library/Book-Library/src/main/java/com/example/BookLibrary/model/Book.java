0a646791e5a87553133d8e955ab86df418956023
Hariharan MS <113448386+Harits77@users.noreply.github.com>
2025-09-18 13:23:24 +0530

    Update Book.java

--- Book Library/Book-Library/src/main/java/com/example/BookLibrary/model/Book.java
+++ Book Library/Book-Library/src/main/java/com/example/BookLibrary/model/Book.java
@@ -1,59 +1,20 @@
 package com.example.BookLibrary.model;
 
+import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Document(collection = "books")
 public class Book {
-
     @Id
     private String id;
-
     private String title;
     private String author;
     private String thumbnail;
-    private String userId; // ✅ Add this field
-
-    // Getters and Setters
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getAuthor() {
-        return author;
-    }
-
-    public void setAuthor(String author) {
-        this.author = author;
-    }
-
-    public String getThumbnail() {
-        return thumbnail;
-    }
-
-    public void setThumbnail(String thumbnail) {
-        this.thumbnail = thumbnail;
-    }
-
-    public String getUserId() {
-        return userId;
-    }
-
-    public void setUserId(String userId) {
-        this.userId = userId;
-    }
-
+    private String userId;
 }
